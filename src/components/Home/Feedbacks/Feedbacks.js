1268a4dacd9779aa5f32fa90dc3f9877d29f0baf
Talal Taki <talaltaki.tt@gmail.com>
2021-07-31 12:19:58 +0600

    Some changes in Feedbacks section

--- src/components/Home/Feedbacks/Feedbacks.js
+++ src/components/Home/Feedbacks/Feedbacks.js
@@ -21,27 +21,15 @@ const Feedbacks = () => {
         <div className="font-cursive fs-1 fw-bold main-text">Feedback</div>
         <div className="fs-3 fw-bold">Clients' Appreciation</div>
       </div>
+      <div className="fs-1 main-text text-center mt-3 mb-3">
+        <FaQuoteLeft />
+      </div>
       <div className="row">
-        <div className="col-md-6">
-          <img
-            src="https://i.ibb.co/6w8kyns/undraw-reviews-lp8w.png"
-            alt=""
-            className="img-fluid"
-            style={{ marginTop: "200px" }}
-          />
-        </div>
-        <div className="col-md-6 col-12">
-          <div className="fs-1 main-text text-center mt-3 mb-2">
-            <FaQuoteLeft />
+        {feedbacks.map((feedback) => (
+          <div className="col-md-3 col-12">
+            <FeedbackCard key={feedback._id} feedback={feedback} />
           </div>
-          <div className="row">
-            {feedbacks.map((feedback) => (
-              <div className="col-md-6 col-12">
-                <FeedbackCard key={feedback._id} feedback={feedback} />
-              </div>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
