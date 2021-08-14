9d8c9db394f75c1f8a580e0e0fc8bc80b4495145
Talal Taki <talaltaki.tt@gmail.com>
2021-08-14 23:00:22 +0600

    Done...

--- src/components/Login/PrivateRoute/PrivateRoute.js
+++ src/components/Login/PrivateRoute/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Redirect, Route } from "react-router-dom";
 import { UserContext } from "../../../App";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const [loggedInUser] = useContext(UserContext);
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
   return (
     <Route
